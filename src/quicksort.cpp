9f72b993a7c5e001de10d2c9ef26f766cbf198d2
edmching <edmching@hotmail.com>
2017-09-21 17:46:51 -0700

    added parallel quicksort

--- src/quicksort.cpp
+++ src/quicksort.cpp
@@ -45,7 +45,13 @@ void quicksort(std::vector<int>& data, int low, int high) {
 // sorts elements low through high (inclusive) using multiple threads
 void parallel_quicksort(std::vector<int>& data, int low, int high) {
   // your code here
-
+	int pivot;
+	if (low < high)
+	{
+		pivot = partition(data, low, high);
+		std::thread t1(&quicksort, data, low, pivot - 1);
+		std::thread t2(&quicksort, data, pivot + 1, high);
+	}
 
 }
 
@@ -61,21 +67,22 @@ int main() {
   std::vector<int> v2 = v1;  // copy all contents
 
   // sort v1 using sequential algorithm
-  quicksort(v1, 0, v1.size()-1);
+ // quicksort(v1, 0, v1.size()-1);
+
+ // sort v2 using parallel algorithm
+  parallel_quicksort(v2, 0, v2.size() - 1);
+
 
   for (int j = 0; j < VECTOR_SIZE-1; ++j)
   {
-	  std::cout << v1[j] << std::endl;
-	  if (v1[j] > v1[j + 1])
+	  std::cout << v2[j] << std::endl;
+	  if (v2[j] > v2[j + 1])
 	  {
 		  std::cout << "the vector is not sorted" << std::endl;
 		  break;
 	  }
 
   }
-
-  // sort v2 using parallel algorithm
-  //parallel_quicksort(v2, 0, v2.size()-1);
 
   return 0;
 }
